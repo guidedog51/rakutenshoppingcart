e96c7eceb4f28291ac4b28dc036c8f51eff0ab12
guidedog51 <nordl2@pacbell.net>
2015-04-05 20:26:40 -0700

    initial commit

---
+++ rakutenshoppingcart/obj/Xcode/12/DerivedData/rakutenshoppingcart/Build/Intermediates/rakutenshoppingcart.build/MonoTouch-iphonesimulator/rakutenshoppingcart.build/Objects-normal/i386/AppDelegate-7D4F3B54FB6F84C2.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/michaelconner/source/repos/rakutenshoppingcart/rakutenshoppingcart/obj/Xcode/12/AppDelegate.m \
+  /Users/michaelconner/source/repos/rakutenshoppingcart/rakutenshoppingcart/obj/Xcode/12/AppDelegate.h
---
+++ rakutenshoppingcart/obj/Xcode/12/DerivedData/rakutenshoppingcart/Build/Intermediates/rakutenshoppingcart.build/MonoTouch-iphonesimulator/rakutenshoppingcart.build/Objects-normal/i386/MasterViewController-1E17F6F5FB3EF9CB.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/michaelconner/source/repos/rakutenshoppingcart/rakutenshoppingcart/obj/Xcode/12/MasterViewController.m \
+  /Users/michaelconner/source/repos/rakutenshoppingcart/rakutenshoppingcart/obj/Xcode/12/MasterViewController.h
